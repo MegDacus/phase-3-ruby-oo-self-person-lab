2b335391728dacd37fcba45a90b79819f84ad14b
Megan Dacus <meganjustice0609@gmail.com>
2023-05-11 13:53:04 +1000

    Refactered code

--- lib/person.rb
+++ lib/person.rb
@@ -9,60 +9,52 @@ class Person
         @hygiene = 8
     end
 
-    def happiness=(value)
-        if value <= 10 && value >= 0
-            @happiness = value
-        elsif value > 10
+    def happiness=(value) 
+        if value > 10
             @happiness = 10
         elsif value < 0
             @happiness = 0
+        else
+            @happiness = value
         end
     end
 
     def hygiene=(value)
-        if value <= 10 && value >= 0
-            @hygiene = value
-        elsif value > 10
+        if value > 10
             @hygiene = 10
         elsif value < 0
             @hygiene = 0
+        else 
+            @hygiene = value
         end
     end
 
     def clean?
-        if hygiene > 7
-            true
-        else
-            false
-        end
+        self.hygiene > 7
     end
 
     def happy?
-        if happiness > 7
-            true
-        else
-            false
-        end
+        self.happiness > 7
     end
 
     def get_paid(amount)
-        self.bank_account = bank_account + amount
+        self.bank_account = self.bank_account + amount
         "all about the benjamins"
     end
 
     def take_bath
-        self.hygiene = hygiene + 4
+        self.hygiene = self.hygiene + 4
         "♪ Rub-a-dub just relaxing in the tub ♫"
     end
 
     def work_out
-        self.happiness = happiness + 2
-        self.hygiene = hygiene - 3
+        self.happiness = self.happiness + 2
+        self.hygiene = self.hygiene - 3
         "♪ another one bites the dust ♫"
     end
 
     def call_friend(friend)
-        self.happiness = happiness + 3
+        self.happiness = self.happiness + 3
         friend.happiness = friend.happiness + 3
         "Hi #{friend.name}! It's #{name}. How are you?"
     end
@@ -70,11 +62,11 @@ class Person
     def start_conversation(friend, topic)
         case topic
         when "politics"
-            self.happiness = happiness - 2
+            self.happiness = self.happiness - 2
             friend.happiness = friend.happiness - 2
             "blah blah partisan blah lobbyist"
         when "weather"
-            self.happiness = happiness + 1
+            self.happiness = self.happiness + 1
             friend.happiness = friend.happiness + 1
             "blah blah sun blah rain"
         else
